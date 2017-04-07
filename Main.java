61b3ad99674587e89a12680e8177ed37f0fa3373
ameen.elhoseiny <ameen.elhoseiny@aurea.com>
2017-04-07 16:33:22 +0200

    adding unused vars

--- Main.java
+++ Main.java
@@ -1,7 +1,10 @@
 public class CaDeadcodeApplication {
     String s;
+    private String unused;
+    private String unused_init = "init";
 
     public static void main(String[] args) {
-        System.out.print("hello");
+        s = "hello";
+        System.out.print(s);
     }
 }
